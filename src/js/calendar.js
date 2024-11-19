d7975f69cdd96e6ec8d88195acbd2684cf84774e
Harry-Pota <ceh2285@gmail.com>
2024-11-19 21:55:02 +0900

    modify swiper, add account info, calendar

---
+++ src/js/calendar.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const datesElement = document.getElementById("dates");
+    const currentMonthElement = document.getElementById("currentMonth");
+
+    const targetMonth = dayjs("2025-03-01"); // 2025년 3월로 고정
+    const selectedDate = dayjs("2025-03-22"); // 기본 선택 날짜
+
+    // 현재 월 표시
+    currentMonthElement.innerHTML = `
+        <div style="font-size: 1.5rem">2025.03.22</div>
+        <div style="margin: 10px auto">토요일 오후 12시</div>
+    `
+
+    // 캘린더 렌더링
+    function renderCalendar() {
+        const startOfMonth = targetMonth.startOf("month");
+        const endOfMonth = targetMonth.endOf("month");
+        const startDate = startOfMonth.startOf("week");
+        const endDate = endOfMonth.endOf("week");
+
+
+        let datesHtml = "";
+        let current = startDate;
+
+        while (current.isBefore(endDate, "day")) {
+            const isCurrentMonth = current.isSame(targetMonth, "month");
+            const isSelected = current.isSame(selectedDate, "day");
+
+            // console.log(current.date())
+            // console.log(current.date() >1)
+            // if(current.date() > 1) {
+            //     datesHtml += `
+            //         <div
+            //           class="date ${isCurrentMonth ? "" : "other-month"} ${isSelected ? "selected" : ""}"
+            //           data-date="${current.format("YYYY-MM-DD")}"
+            //         >
+            //         </div>
+            //       `;
+            // } else {
+            //     datesHtml += `
+            //         <div
+            //           class="date ${isCurrentMonth ? "" : "other-month"} ${isSelected ? "selected" : ""}"
+            //           data-date="${current.format("YYYY-MM-DD")}"
+            //         >
+            //           ${current.date()}
+            //         </div>
+            //       `;
+            // }
+
+            datesHtml += `
+                    <div 
+                      class="date ${isCurrentMonth ? "" : "other-month"} ${isSelected ? "selected" : ""}" 
+                      data-date="${current.format("YYYY-MM-DD")}"
+                    >
+                      ${current.date()}
+                    </div>
+                  `;
+
+            current = current.add(1, "day");
+        }
+
+        datesElement.innerHTML = datesHtml;
+    }
+
+    renderCalendar();
+});
--- src/js/common.js
+++ src/js/common.js
@@ -45,15 +45,103 @@ if (mobileOs.myMobileOs === "etc") {
 
 let swiperDivsion = "";
 
-for (let i = 0; i <= 2; i++) {
-  swiperDivsion += `<div class="swiper-slide"><img id="swiper-image${
-    i + 1
-  }" src="./public/assets/swiper_image${i + 1}.jpeg" alt="swiper_image${
-    i + 1
-  }" height="auto"/></div>`;
+for (let i = 1; i <= 20; i++) {
+  swiperDivsion += `<div class="swiper-slide"><img id="swiper-image${i}" src="./public/assets/swiper_image${i}.jpeg" alt="swiper_image${i}" height="auto"/></div>`;
 }
 
+function clickEvent (e) {
+  const $className = e.nextSibling.nextSibling.className;
+
+  if($(`.${$className}`).css("display") == "none") {
+    $(`.${$className}`).slideDown();
+  } else {
+    $(`.${$className}`).slideUp();
+  }
+}
+
+// 축하의 마음 전하기
+let groomsAccount = "";
+let brideAccount = "";
+
+
+const groomsAccountInfos = [
+  {name: "신랑 최은호", className: "groomAccount", account: "농협 821094-56-013826", phoneNum: "010-9559-3525"},
+  {name: "부) 최명규", className: "groomAccountF", account: "신한은행 110-543-320411", phoneNum: "010-2849-2285"},
+  {name: "모) 이연순", className: "groomAccountM", account: "카카오뱅크 3333-19-9692101", phoneNum: "010-4542-2285"}
+];
+
+const brideAccountInfos = [
+  {name: "신부 김해니", className: "_brideAccount", account: "신한은행 110-476-947623", phoneNum: "010-5174-0677"},
+  {name: "부) 김진광", className: "_brideAccountF", account: "국민은행 217-24-0125-056", phoneNum: "010-5087-0678"},
+  {name: "모) 김영미", className: "_brideAccountM", account: "국민은행 217-21-0458-402", phoneNum: "010-5063-0674"}
+];
+
+groomsAccountInfos.forEach((rendor) => (
+    groomsAccount += `
+    <div class="account-name" onclick="clickEvent(this)" style="${!rendor.name.includes("신랑") && "padding-left: 2rem"}">
+      ${rendor.name}
+    </div>
+    <div class=${rendor.className} style="display: none; height: auto; border: 1px solid black">
+       <!-- 계좌번호 -->
+      <div
+       style="${!rendor.name.includes("신랑") ? "padding: 0.5rem 0.5rem 0.5rem 2rem" : "padding: 0.5rem 0.5rem 0.5rem 1rem"}"
+       onclick="copyAccount(this)"
+      >${rendor.account}</div>
+      <hr style="margin: 0"/>
+      <!-- 전화번호 -->
+      <div onclick="location.href='sms:${rendor.phoneNum}'"
+           style="${!rendor.name.includes("신랑") ? "padding: 0.5rem 0.5rem 0.5rem 2rem" : "padding: 0.5rem 0.5rem 0.5rem 1rem"}"
+      >
+        ${rendor.phoneNum}
+      </div>
+    </div>
+ `
+))
+
+brideAccountInfos.forEach((rendor) => (
+  brideAccount += `
+    <div class="account-name" onclick="clickEvent(this)" style="${!rendor.name.includes("신부") && "padding-left: 2rem"}">
+      ${rendor.name}
+    </div>
+    <div class=${rendor.className} style="display: none; height: auto; border: 1px solid black">
+       <!-- 계좌번호 -->
+      <div
+       style="${!rendor.name.includes("신부") ? "padding: 0.5rem 0.5rem 0.5rem 2rem" : "padding: 0.5rem 0.5rem 0.5rem 1rem"}"
+       onclick="copyAccount(this)"
+      >${rendor.account}</div>
+      <hr style="margin: 0"/>
+      <!-- 전화번호 -->
+      <div onclick="location.href='sms:${rendor.phoneNum}'"
+           style="${!rendor.name.includes("신부") ? "padding: 0.5rem 0.5rem 0.5rem 2rem" : "padding: 0.5rem 0.5rem 0.5rem 1rem"}"
+      >
+        ${rendor.phoneNum}
+      </div>
+    </div>
+ `
+))
+
+// 계좌번호 복사 Function
+function copyAccount(e) {
+  const $account = e.innerText;
+
+  const textArea = document.createElement("textarea");
+  textArea.value =  $account;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    document.execCommand('copy');
+  } catch (err) {
+    console.error('Unable to copy to clipboard', err);
+  }
+  document.body.removeChild(textArea);
+
+  window.alert("복사되었습니다.");
+};
+
 $(".swiper-wrapper").append(swiperDivsion);
+$(".grooms").append(groomsAccount);
+$(".brides").append(brideAccount);
 
 $(".main-name.left").text(weddingInfo.bride.name);
 $(".main-name.right").text(weddingInfo.groom.name);
@@ -61,3 +149,4 @@ $(".main-wedding-date").text(weddingInfo.date);
 $(".main-wedding-venu").text(weddingInfo.venu);
 $(".location-info").text(weddingInfo.venu);
 $(".location-info-address").text(weddingInfo.address);
+
